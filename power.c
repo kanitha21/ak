2eb5e1b9619fab3d2787d38c3da91933b9707b45
kanitha21 <48172759+kanitha21@users.noreply.github.com>
2019-05-21 02:52:33 +0530

    Create power.c

---
+++ power.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<math.h>
+void main()
+{
+    int a,b,c=1,i=1;
+    scanf("%d %d",&a,&b);
+    while(i<=b)
+    {
+        c=c*a;
+    i++;
+     }
+    
+    printf("%d",c);
+}
