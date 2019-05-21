bae3278c2215daf3aca0b613aa68c9e89677ebe1
kanitha21 <48172759+kanitha21@users.noreply.github.com>
2019-05-21 19:29:32 +0530

    Create numeric.c

---
+++ numeric.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<ctype.h>
+void main()
+{
+    int n;
+    scanf("%d",&n);
+    if(!isdigit(n))
+    {
+        printf("yes");
+    }
+    else 
+    {
+        printf("no");
+    }
+}
