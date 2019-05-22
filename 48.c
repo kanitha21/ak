1696777e5ce8daf6cc72fb42e83ddbdbd4e6df6f
kanitha21 <48172759+kanitha21@users.noreply.github.com>
2019-05-22 21:01:47 +0530

    Create 48.c

---
+++ 48.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+    int n,a[100],i,sum=0,avg=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        sum+=a[i];
+    }
+    avg=sum/n;
+printf("%d",avg);
+}
