f3299ecd763b03b8f51b82b737a54e58cf2a3b12
kanitha21 <48172759+kanitha21@users.noreply.github.com>
2019-05-22 15:36:54 +0530

    Create maxarray.c

---
+++ maxarray.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+int max=0,a[10],i;
+for(i=0;i<10;i++)
+{
+    scanf("%d",&a[i]);
+}
+max=a[0];
+  for(i=0;i<10;i++)
+  {
+      if(max<a[i])
+      {
+          max=a[i];
+      }
+  }
+  printf("%d",max);
+}
