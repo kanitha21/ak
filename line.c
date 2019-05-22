7302d461fcacb55575251ee203032dda1fa8725d
kanitha21 <48172759+kanitha21@users.noreply.github.com>
2019-05-22 14:39:49 +0530

    Create line.c

---
+++ line.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+int n,s=0,i,r;
+char a[50];
+gets(a);
+n=strlen(a);
+for(i=0;i<n;i++)
+{
+if(a[i] == '.')
+{
+     s++;
+}
+}
+printf("%d",s);
+}
