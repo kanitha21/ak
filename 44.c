0e5cad2999a468fa7a53bd6ac3dbd91e7265d905
kanitha21 <48172759+kanitha21@users.noreply.github.com>
2019-05-22 20:28:41 +0530

    Create 44.c

---
+++ 44.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+    int n,i;
+    scanf("%d",&n);
+     if(n==1||n==2||n==3||n==4||n==5||n==6||n==7||n==8||n==9|| n==10)
+     {
+         printf("yes");
+     }
+      else
+      {
+          printf("no");
+ }
+}
+  
