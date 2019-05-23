998d92667c5cdb5dff1c75724faa3de052071559
kanitha21 <48172759+kanitha21@users.noreply.github.com>
2019-05-23 12:56:51 +0530

    Create 70.c

---
+++ 70.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<math.h>
+void main()
+{
+    int n,i,p=0;
+    scanf("%d",&n);
+    for(i=1;i<n/2;i++)
+    {
+        if(n==pow(2,i))
+        {
+            p=++i;
+            break;
+        }
+    }
+    printf("%d",pow(2,p));
+}
