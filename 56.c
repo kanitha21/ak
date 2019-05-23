e71aafcbf20a6f0e4c8795837698740778ccc6e9
kanitha21 <48172759+kanitha21@users.noreply.github.com>
2019-05-23 09:00:02 +0530

    Create 56.c

---
+++ 56.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<ctype.h>
+#include<string.h>
+void main()
+{
+  char a[50],n,i,c=0;
+  gets(a);
+  n=strlen(a);
+  for(i=0;i<n;i++)
+  {
+      if(isalnum(a[i]))
+      {
+          c++;
+      }
+     }
+    if(n==c)
+    {
+        printf("yes");
+    }
+    else
+    {
+        printf("no");
+    }
+  
+}
