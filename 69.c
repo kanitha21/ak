25268e98cad9a40597c4bbac93a85766581f99ad
kanitha21 <48172759+kanitha21@users.noreply.github.com>
2019-05-23 12:33:47 +0530

    Create 69.c

---
+++ 69.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+    int n,m,s;
+        scanf("%d %d",&n,&m);
+        s=n-m;
+        if(s%2==0)
+        {
+            printf("even");
+        }
+          else
+          {
+              printf("odd");
+          }
+}
+© 2019 GitHub, Inc.
