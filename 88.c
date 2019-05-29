7f9840d4b29480786a3b18187c862b96ae5b1cc8
kanitha21 <48172759+kanitha21@users.noreply.github.com>
2019-05-29 16:19:15 +0530

    Update 88.c

--- 88.c
+++ 88.c
@@ -5,7 +5,7 @@ void main()
     scanf("%d %d",&a,&b);
     for(i=1; i <=a && i <=b; ++i)
     {
-        if(a%i==0 && a%i==0)
+        if(a%i==0 && b%i==0)
             gcd = i;
     }
     lcm=(a*b)/gcd;
