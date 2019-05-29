171dd7a55896415c8b7b677af99d2fc34cc6383c
kanitha21 <48172759+kanitha21@users.noreply.github.com>
2019-05-29 19:19:50 +0530

    Update 104.c

--- 104.c
+++ 104.c
@@ -1,9 +1,12 @@
 #include<stdio.h>
-#include<string.h>
 void main()
 {
-    int n,k,s;
+    int n,k,s=1;
     scanf("%d %d",&n,&k);
-    s=n^k;
+    while(k!=0)
+    {
+        s*=n;
+        k--;
+    }
     printf("%d",s);
 }
