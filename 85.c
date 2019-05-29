71b72bab0835633a9b9cf40703a629093569f31f
kanitha21 <48172759+kanitha21@users.noreply.github.com>
2019-05-29 15:23:56 +0530

    Create 85.c

---
+++ 85.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+    int i;
+    char a[20];
+   gets(a);
+   for(i=0;i<strlen(a);i++)
+   {
+       if(i%2==0)
+  {
+   printf("%c",a[i]);
+}
+}
+for(i=0;i<strlen(a);i++)
+   {
+       if(i%2!=0)
+  {
+   printf("%c",a[i]);
+}
+}
+}
