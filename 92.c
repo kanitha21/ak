dcf0331ef4e6ff85f604634a0391d9e12fe9083a
kanitha21 <48172759+kanitha21@users.noreply.github.com>
2019-05-29 16:52:59 +0530

    Create 92.c

---
+++ 92.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+    int n,a[20],sum=0,i;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+     for(i=0;i<n;i++)
+    {
+       sum+=a[i];
+    }
+    printf("%d",sum);
+}
+
