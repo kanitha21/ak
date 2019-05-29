6d8ca22aa80147167c44b5ac3bb62237a2a66433
kanitha21 <48172759+kanitha21@users.noreply.github.com>
2019-05-29 18:15:21 +0530

    Create 98.c

---
+++ 98.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+    int n,a[10],k,i;
+    scanf("%d",n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+    if(a[i]>a[i+1])
+    {
+        printf("%d",i);
+    }
+}
+}
+
